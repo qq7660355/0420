ee8727aa37ba8506a5ac281cf2f59678126fe2b5
qq7660355 <36753199+qq7660355@users.noreply.github.com>
2018-04-20 02:31:05 +0800

    123

---
+++ Assets/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1) * Time.deltaTime*speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1) * Time.deltaTime*speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime*speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime*speed;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -5, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 5, 0);
+        }
+    }
+}
